60f1d66876bd4b1a5c98cb290e30b3861f4e1d20
Richard Bamford <rbamfordz@gmail.com>
2020-02-29 14:17:40 +0000

    initial website

--- pico_src/alert.cpp
+++
@@ -1,8 +0,0 @@
-#include "../pico_inc/alert.h"
-
-void alert(const char * inMessage, uint32_t inLength)
-{
-    uintptr_t strPtr = reinterpret_cast<uintptr_t>(inMessage);
-
-    _alert(strPtr, inLength);
-}
--- pico_src/conversion.cpp
+++
@@ -1,8 +0,0 @@
-#include "../pico_inc/conversion.h"
-
-void To_Char_Array(uint32_t inNumber, char * outDestinationPtrLocation, uint32_t outDestinationByteLength)
-{
-    uintptr_t destPtr = reinterpret_cast<uintptr_t>(outDestinationPtrLocation);
-
-    _To_Char_Array(inNumber, destPtr, outDestinationByteLength);
-}
