54657499b16d6f401d47b32e44433fd047f8ed19
Bambofy <rbamfordz@gmail.com>
2020-02-29 20:21:34 +0000

    added llvm binaries for windows

--- build.bat
+++ build.bat
@@ -1,9 +1,6 @@
-set llvmbindir=E:\llvm-9.0.1.src\build\Release\bin
-set clangdir=C:\Program Files\LLVM\bin
-
-Call "%clangdir%\clang++.exe" -cc1 -emit-llvm-bc -triple=wasm32-unknown-unknown-wasm -std=c++98 -Weverything -fvisibility hidden %cd%/pico_src/*.cpp %cd%/app/src/*.cpp
+Call "%cd%/llvm/clang++.exe" -cc1 -emit-llvm-bc -triple=wasm32-unknown-unknown-wasm -std=c++98 -Weverything -fvisibility hidden %cd%/pico_src/*.cpp %cd%/app/src/*.cpp
 
 
-Call "%llvmbindir%\llvm-link.exe" -o wasmprog.bc pico_src/*.bc app/src/*.bc
-Call "%llvmbindir%\llc.exe" -O3 -filetype=obj wasmprog.bc -o wasm.o
-Call "%llvmbindir%\wasm-ld.exe" --no-entry wasm.o -o picobin.wasm --strip-all -allow-undefined-file pico.syms --import-memory --export-dynamic
+Call "%cd%/llvm/llvm-link.exe" -o wasmprog.bc pico_src/*.bc app/src/*.bc
+Call "%cd%/llvm/llc.exe" -O3 -filetype=obj wasmprog.bc -o wasm.o
+Call "%cd%/llvm/wasm-ld.exe" --no-entry wasm.o -o picobin.wasm --strip-all -allow-undefined-file pico.syms --import-memory --export-dynamic
